c79a6a998c96417e6e1382e7c7a448383eceea86
andrey.tazov <andrey.tazov@g5e.com>
2021-07-20 10:18:43 +0300

    Add data-layer

--- src/services/ApiClient.js
+++ src/services/ApiClient.js
@@ -1,18 +1,23 @@
 class ApiClient {
-
     static SERVER_URL = 'http://localhost:8080';
     static GET_CHALLENGE = '/challenges/random';
     static POST_RESULT = '/attempts';
+    static GET_ATTEMPTS_BY_ALIAS = '/attempts?alias=';
 
     static challenge(): Promise<Response> {
         return fetch(ApiClient.SERVER_URL + ApiClient.GET_CHALLENGE);
     }
 
-    static sendGuess(user: string, a: number, b: number, guess: number): Promise<Response> {
+    static sendGuess(user: string,
+                     a: number,
+                     b: number,
+                     guess: number): Promise<Response> {
         return fetch(ApiClient.SERVER_URL + ApiClient.POST_RESULT,
             {
                 method: 'POST',
-                headers: {'Content-Type': 'application/json'},
+                headers: {
+                    'Content-Type': 'application/json'
+                },
                 body: JSON.stringify(
                     {
                         userAlias: user,
@@ -23,6 +28,12 @@ class ApiClient {
                 )
             });
     }
+
+    static getAttempts(userAlias: string): Promise<Response> {
+        console.log('Get attempts for '+userAlias);
+        return fetch(ApiClient.SERVER_URL + ApiClient.GET_ATTEMPTS_BY_ALIAS + userAlias);
+    }
 }
 
 export default ApiClient;
+
